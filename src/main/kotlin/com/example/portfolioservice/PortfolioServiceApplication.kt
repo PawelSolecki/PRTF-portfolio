dc709c3bb66a4587a0eec91fbcc34fd6be48e5ef
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2025-02-06 17:03:09 +0100

    removed redundant @

--- src/main/kotlin/com/example/portfolioservice/PortfolioServiceApplication.kt
+++ src/main/kotlin/com/example/portfolioservice/PortfolioServiceApplication.kt
@@ -2,10 +2,8 @@ package com.example.portfolioservice
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("com.example.portfolioservice")
 class PortfolioServiceApplication
 
 fun main(args: Array<String>) {
