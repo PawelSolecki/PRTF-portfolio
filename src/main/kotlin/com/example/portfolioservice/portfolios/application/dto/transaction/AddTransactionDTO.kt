14100fc734b98f3f7acb6d69270a763823b4766e
PawelS <58605803+PawelSolecki@users.noreply.github.com>
2025-03-30 18:01:59 +0200

    add date to AddTransactionDTO

--- src/main/kotlin/com/example/portfolioservice/portfolios/application/dto/transaction/AddTransactionDTO.kt
+++ src/main/kotlin/com/example/portfolioservice/portfolios/application/dto/transaction/AddTransactionDTO.kt
@@ -3,6 +3,7 @@ package com.example.portfolioservice.portfolios.application.dto.transaction
 import com.example.portfolioservice.portfolios.domain.model.Transaction
 import com.example.portfolioservice.portfolios.domain.model.TransactionType
 import java.math.BigDecimal
+import java.time.LocalDateTime
 import java.util.*
 
 data class AddTransactionDTO(
@@ -10,13 +11,15 @@ data class AddTransactionDTO(
     val type: TransactionType,
     val quantity: BigDecimal,
     val pricePerUnit: BigDecimal,
+    val date: LocalDateTime? = LocalDateTime.now()
 ){
     fun toDomain(): Transaction {
         return Transaction(
             assetId = assetId,
             type = type,
             quantity = quantity,
-            pricePerUnit = pricePerUnit
+            pricePerUnit = pricePerUnit,
+            date = date
         )
     }
 }
